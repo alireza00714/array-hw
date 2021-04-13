7838a49c62f596e2a3f54c6e3027de1adb7c1af2
Alireza Pirmozhdehi <zootel.land@gmail.comi>
2021-04-13 10:21:47 +0430

    solve question 4

--- index.js
+++ index.js
@@ -15,5 +15,18 @@ let combine = function (arr, ...arrays) {
   }
 
   arr.sort((a, b) => a - b);
+
   return arr;
+};
+
+//solution 4
+let numberFinder = function (text) {
+  number = [];
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] == " ") continue;
+    if (typeof (+text[i]) == "number" && isNaN(+text[i]) == false) {
+      number.push(+text[i]);
+    } else continue;
+  }
+  return number;
 };
