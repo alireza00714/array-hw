5449a270cf768ea63d3c751cc210e62148430fe9
Alireza Pirmozhdehi <zootel.land@gmail.comi>
2021-04-13 10:42:51 +0430

    solve question 5

--- index.js
+++ index.js
@@ -24,9 +24,24 @@ let numberFinder = function (text) {
   number = [];
   for (let i = 0; i < text.length; i++) {
     if (text[i] == " ") continue;
-    if (typeof (+text[i]) == "number" && isNaN(+text[i]) == false) {
+    if (typeof +text[i] == "number" && isNaN(+text[i]) == false) {
       number.push(+text[i]);
     } else continue;
   }
   return number;
 };
+
+//solution 5
+let flat = [];
+let arrayFlatter = function (arr) {
+  for (let item of arr) {
+    if (Array.isArray(item) == true) arrayFlatter(item);
+    else {
+      flat.push(item);
+      continue;
+    }
+  }
+  return flat;
+};
+
+console.log(arrayFlatter([5, [2, 3], [[[2, 9], 4], 4], 1, 0]));
