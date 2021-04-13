a07689553ba64faf916f78fb26fc81ec92ef6db1
Alireza Pirmozhdehi <zootel.land@gmail.comi>
2021-04-13 08:50:38 +0430

    solve question 2

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 "strict mode";
 
-let arr = ["a", "b", "c", "d"];
-
 // solution 1
 let deleteByIndex = function (arr, index) {
   arr.splice(index, 1);
   return arr;
 };
 
+//solution 2
+let combine = function (arr, ...arrays) {
+  for (let item1 of arrays) {
+    for (let item2 of item1) {
+      arr.push(item2);
+    }
+  }
+
+  arr.sort((a, b) => a - b);
+  return arr;
+};
